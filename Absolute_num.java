d2234f65a6bea5ff42a4004ef3935b2b5d538a95
digbijay1 <55594240+digbijay1@users.noreply.github.com>
2020-10-03 19:10:41 +0530

    Create Absolute_num.java

---
+++ Absolute_num.java
@@ -0,0 +1,12 @@
+public class HelloWorld{
+
+     public static void main(String []args){
+        int a=45;
+        if(a<0){
+            System.out.println((-1)*a);
+        }
+        else{
+            System.out.println(a);
+        }
+     }
+}
